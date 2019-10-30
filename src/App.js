980d9119d98f7d3f71f2d34be54dc99c2ee194f9
Eric Takahashi <eric.takahashi@wipro.com>
2019-10-30 18:04:53 -0300

    Route: Set home and checkout routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -8,8 +9,10 @@ function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,31 +85,33 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
+    // this.setState({loading: true});
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Eric',
-        address: {
-          street: 'Test Street A',
-          zipCode: '12345',
-          country: 'Brazil'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    };
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Eric',
+    //     address: {
+    //       street: 'Test Street A',
+    //       zipCode: '12345',
+    //       country: 'Brazil'
+    //     },
+    //     email: 'test@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // };
 
-    // For firebase it is necessary to add the .json extension.
-    axios.post('orders.json', order)
-      .then(response => {
-        this.setState({loading: true, purchasing: false});
-      })
-      .catch(error => {
-        this.setState({loading: true, purchasing: false});
-      });
+    // // For firebase it is necessary to add the .json extension.
+    // axios.post('orders.json', order)
+    //   .then(response => {
+    //     this.setState({loading: true, purchasing: false});
+    //   })
+    //   .catch(error => {
+    //     this.setState({loading: true, purchasing: false});
+    //   });
+
+    this.props.history.push('/checkout');
   }
 
   render () {
