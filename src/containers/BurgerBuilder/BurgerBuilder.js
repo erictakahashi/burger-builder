bb8632bcc5abb42974cc5f7435e66cf5cc255bc9
Eric Takahashi <eric.takahashi@wipro.com>
2019-10-31 15:08:12 -0300

    ContactData: Post orders to the server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,44 +85,19 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // this.setState({loading: true});
-
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Eric',
-    //     address: {
-    //       street: 'Test Street A',
-    //       zipCode: '12345',
-    //       country: 'Brazil'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // };
-
-    // // For firebase it is necessary to add the .json extension.
-    // axios.post('orders.json', order)
-    //   .then(response => {
-    //     this.setState({loading: true, purchasing: false});
-    //   })
-    //   .catch(error => {
-    //     this.setState({loading: true, purchasing: false});
-    //   });
-
-
     // Alternative Solution
     // const queryParams = [];
     // for (let i in this.state.ingredients) {
     //   queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     // }
+    // queryParams.push('price=' + this.state.totalPrice);
     // const queryString = queryParams.join('&');
 
     let queryString = Object.keys(this.state.ingredients)
       .reduce((query, ingredientKey, index) => {
         return query + ((index === 0) ? '?' : '&') + encodeURIComponent(ingredientKey) + '=' + encodeURIComponent(this.state.ingredients[ingredientKey]);
       }, '');
+    queryString += '&price=' + this.state.totalPrice;
 
     this.props.history.push({
       pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
+import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import styles from './ContactData.module.css';
 
 class ContactData extends Component {
@@ -11,21 +13,60 @@ class ContactData extends Component {
       zipCode: "12345"
     },
     email: "test@test.com",
-    name: "Eric"
+    name: "Eric",
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.totalPrice,
+      customer: {
+        name: 'Eric',
+        address: {
+          street: 'Test Street A',
+          zipCode: '12345',
+          country: 'Brazil'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+
+    // For firebase it is necessary to add the .json extension.
+    axios.post('orders.json', order)
+      .then(response => {
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
   }
 
   render() {
+    let form = (
+      <form>
+        <input className={styles.Input} type="text" name="name" placeholder="Your Name" />
+        <input className={styles.Input} type="email" name="email" placeholder="Your Email" />
+        <input className={styles.Input} type="text" name="street" placeholder="Street" />
+        <input className={styles.Input} type="text" name="zipcode" placeholder="Zip Code" />
+        <input className={styles.Input} type="text" name="country" placeholder="Country" />
+        <Button type="Success" clicked={this.orderHandler}>Order</Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />
+    }
+
     return (
       <div className={styles.ContactData}>
         <h4>Enter your contact data:</h4>
-        <form>
-          <input className={styles.Input} type="text" name="name" placeholder="Your Name" />
-          <input className={styles.Input} type="email" name="email" placeholder="Your Email" />
-          <input className={styles.Input} type="text" name="street" placeholder="Street" />
-          <input className={styles.Input} type="text" name="zipcode" placeholder="Zip Code" />
-          <input className={styles.Input} type="text" name="country" placeholder="Country" />
-          <Button type="Success">Order</Button>
-        </form>
+        {form}
       </div>
     );
   }
