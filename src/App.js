cf5ecc010b44351b8cf4d8148ff45e3595d7e5c8
Eric Takahashi <eric.takahashi@wipro.com>
2019-10-31 16:08:12 -0300

    Orders: Create a orders page with hardcoded data and set nav links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Layout>
         <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" component={BurgerBuilder} />
         </Switch>
       </Layout>
